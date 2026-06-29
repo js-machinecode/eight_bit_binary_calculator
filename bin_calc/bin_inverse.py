2ae3dc75be8789b6fc6907538b887c03cb2e9043
js-machinecode <jsoto.machinecode@gmail.com>
2026-06-29 16:28:13 -0400

    Updated func. with a doc string

--- bin_calc/bin_inverse.py
+++ bin_calc/bin_inverse.py
@@ -1,4 +1,5 @@
 from type_defs import EightBitBinaryNumber
 
 def bin_inverse(bin_str: EightBitBinaryNumber) -> EightBitBinaryNumber:
+    '''Convert bin_str to it's opposite (one's complement)'''
     raise NotImplementedError
