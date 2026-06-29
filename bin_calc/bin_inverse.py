0b137a3a2cee5a3e1317c7fa6cec0f3c16afb74e
js-machinecode <jsoto.machinecode@gmail.com>
2026-06-29 17:02:24 -0400

    made function body for bin_inverse

--- bin_calc/bin_inverse.py
+++ bin_calc/bin_inverse.py
@@ -2,4 +2,8 @@ from bin_calc.type_defs import EightBitBinaryNumber
 
 def bin_inverse(bin_str: EightBitBinaryNumber) -> EightBitBinaryNumber:
     '''Convert bin_str to it's opposite (one's complement)'''
-    raise NotImplementedError
+    desired_bin_str = ''
+    for bit in bin_str:
+        flipped_bit = '1' if bit == "0" else '0'
+        desired_bin_str += flipped_bit
+    return desired_bin_str
