231c7f819fdf71875c77ee854303f86d0676120a
js-machinecode <jsoto.machinecode@gmail.com>
2026-06-29 17:21:06 -0400

    made func. body

--- bin_calc/twos_complement.py
+++ bin_calc/twos_complement.py
@@ -1,5 +1,9 @@
 from bin_calc.type_defs import EightBitBinaryNumber
+from bin_calc.arithmetic import add_bin
+from bin_calc.bin_inverse import bin_inverse
+
+ONE = '00000001'
 
 def twos_complement(bin_str: EightBitBinaryNumber) -> EightBitBinaryNumber: 
     '''Add binary one to a eight bit binary number'''
-    raise NotImplementedError
+    return add_bin(ONE, bin_inverse(bin_str))
